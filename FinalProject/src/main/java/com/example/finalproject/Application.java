84e5b56e991d3788c0b0270a7997d0229c92df84
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2022-11-19 09:15:16 -0800

    UI

---
+++ FinalProject/src/main/java/com/example/finalproject/Application.java
@@ -0,0 +1,23 @@
+package com.example.finalproject;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        scene.getStylesheets().add("styles.css");
+        stage.setTitle("Image Management Tool");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
